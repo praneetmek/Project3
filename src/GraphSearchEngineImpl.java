67713ccbc505811dbbddde4832e3b27dd18c8e72
Shreyashee Bhattacharjee <shreyasheeb@gmail.com>
2018-11-14 16:44:39 -0500

    BFS_v1.2

--- src/GraphSearchEngineImpl.java
+++ src/GraphSearchEngineImpl.java
@@ -1,20 +1,25 @@
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 
 public class GraphSearchEngineImpl implements  GraphSearchEngine {
 // Data
+    private int _distance;
     private Node _start;
     private Node _end;
     private ArrayList<Node> _BFSList;
+    private HashMap<Node, Integer> _BFSMap;
     private ArrayList<Node> _connectionList; //degree of the connection is the length-1
 
 
 // Constructor
     public GraphSearchEngineImpl() {
+        _distance = 0;
         _start = null;
         _end = null;
         _BFSList = new ArrayList<Node>();
+        _BFSMap = new HashMap<Node, Integer>();
         _connectionList = new ArrayList<Node>();
     }
 
@@ -31,10 +36,13 @@ public class GraphSearchEngineImpl implements  GraphSearchEngine {
     public List<Node> findShortestPath (Node s, Node t) {
         _start = s;
         _end = t;
+        _BFSMap.put(_start, _distance);
         _BFSList.add(_start);
         _connectionList.add(_end);
-        while(_BFSList.contains(t) != true)
+        while(_BFSMap.containsValue(t) != true) {
+            _distance++;
             getNextNodeSet();
+        }
         backTrack();
         return _connectionList;
     }
@@ -42,6 +50,9 @@ public class GraphSearchEngineImpl implements  GraphSearchEngine {
     private void getNextNodeSet() {
         for(Node node: _BFSList) {
             _BFSList.addAll(node.getNeighbors());
+            for(Node neighbor : node.getNeighbors()) {
+                _BFSMap.put(neighbor, _distance);
+            }
         }
     }
 
@@ -49,7 +60,7 @@ public class GraphSearchEngineImpl implements  GraphSearchEngine {
         while(_connectionList.contains(_start) != true) {
             for (Node node : _connectionList) {
                 for(Node neighbor : node.getNeighbors()) {
-                    if (_BFSList.contains(neighbor)) {   // could also remove from _bfslist bc less space but more time??
+                    if (_BFSMap.containsValue(neighbor) && _BFSMap.get(neighbor) == _BFSMap.get(_end)-1) {   // could also remove from _bfslist bc less space but more time??
                         _connectionList.add(neighbor);
                         break;
                     }
